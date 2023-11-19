c0a43db5619b74549f38f6d800f2da91e1b14f78
Luca Pavan <hello@lucapavan.dev>
2023-11-19 19:59:07 +0100

    Added release workflow configuration
    Updated the database seeder file to include additional fields in the User factory, and added a new GitHub workflow file and php.ini for enhanced CI/CD pipeline.
    
    In the database seeder, fields for `email_verified_at`, `password`, and `remember_token` were added to the User insert method for more complete user data seeding.
    
    The newly added GitHub workflow `release.yml` is designed to set up the PHP environment, create database, install dependencies, prepare environment, migrate and seed the database, cache application, and create a release.
    
    The introduction of a new `php.ini` file provides a configuration for PHP script execution like setting memory limits, error reporting, data handling etc.
    
    These changes enhance the efficiency of the development process by improving the database seeding process and allowing for automated workflows.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory()->create([
+         \App\Models\User::insert([
              'name' => 'User',
              'email' => 'user@example.com',
+             'email_verified_at' => now(),
+             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+             'remember_token' => Str::random(10),
          ]);
     }
 }
